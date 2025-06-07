7133181562acae000cb758858c05d8b8cae03221
sanjay <sanjay13649@gmail.com>
2025-06-07 22:00:51 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,11 +2,11 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform => const FirebaseOptions(
-    apiKey: 'AIzaSyCxa2v_NYFO4FPwTZr6UU5s4ym2NQ2uPQ0',
-    appId: '1:748298667757:android:8cb8caa6a6b7ab7be32ee4',
-    messagingSenderId: '748298667757',
-    projectId: 'fixr-6d1a8',
-    databaseURL: 'https://fixr-6d1a8-default-rtdb.firebaseio.com',
-    storageBucket: 'fixr-6d1a8.firebasestorage.app',
+    apiKey: '*********************************',
+    appId: '*********************************',
+    messagingSenderId: '*********************************',
+    projectId: '*********************************',
+    databaseURL: '*********************************',
+    storageBucket: '*********************************',
   );
 }
